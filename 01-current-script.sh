d21db1bdb3ddf5482b0d9131ce598a8d3db86595
NarendraReddy <narendra.nya@gmail.com>
2024-04-27 14:45:02 -0700

    calling

--- 01-current-script.sh
+++ 01-current-script.sh
@@ -4,7 +4,7 @@ COURSE="Devops in current script"
 echo "Before calling the other script the variable : course value::::::$COURSE"
 echo "Before calling other script, the pid of current script:::::::::$$"
 
-./02-other-script1.sh
+./02-other-script.sh
 
 echo "After calling the other script the variable : course value:::::$COURSE"
 
